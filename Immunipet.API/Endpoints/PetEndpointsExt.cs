f72a002828646ef46e2c194589758662cdf93900
Victor Bastos <vbastosmaciel@outlook.com.br>
2024-06-29 18:34:34 -0300

    feat: Finishing pets endpoints

--- Immunipet.API/Endpoints/PetEndpointsExt.cs
+++ Immunipet.API/Endpoints/PetEndpointsExt.cs
@@ -1,4 +1,5 @@
 using Immunipet.API.Data;
+using Immunipet.API.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Immunipet.API.Endpoints;
@@ -15,9 +16,38 @@ public static class PetEndpointsExt
             return pet is not null ? Results.Ok(pet) : Results.NotFound();
         });
 
-        app.MapPost("/pet", async (AppDbContext db) =>
+        app.MapPost("/pet", async (AppDbContext db, Pet pet) =>
         {
+            db.Pets.Add(pet);
+            await db.SaveChangesAsync();
 
+            return Results.Created($"/pet/{pet.Id}", pet);
+        });
+
+        app.MapPut("/pet/{id}", async (AppDbContext db, int id, Pet pet) => 
+        {
+            var findPet = await db.Pets.FindAsync(id);
+
+            if (findPet is null) return Results.NotFound();
+
+            pet.Name = findPet.Name;
+            pet.Birthday = findPet.Birthday;
+            pet.Age = findPet.Age;
+
+            await db.SaveChangesAsync();
+            return Results.NoContent();
+        }); 
+
+        app.MapDelete("/pet/{id}", async (AppDbContext db, int id) => 
+        {
+            if (await db.Pets.FindAsync(id) is Pet pet)
+            {
+                db.Pets.Remove(pet);
+                await db.SaveChangesAsync();
+                return Results.NoContent();
+            }
+
+            return Results.NotFound();
         });
     }
 }
